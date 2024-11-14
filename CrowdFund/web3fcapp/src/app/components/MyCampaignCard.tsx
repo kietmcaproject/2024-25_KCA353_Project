e809855a64a4c20fd94a369990133ef014c35157
Govind Gupta <gvgupta789@gmail.com>
2024-11-14 09:46:28 +0530

    commit main

---
+++ CrowdFund/web3fcapp/src/app/components/MyCampaignCard.tsx
@@ -0,0 +1,51 @@
+import { client } from "@/app/client";
+import Link from "next/link";
+import { getContract } from "thirdweb";
+import { lineaSepolia } from "thirdweb/chains";
+import { useReadContract } from "thirdweb/react";
+
+type MyCampaignCardProps = {
+    contractAddress: string;
+};
+
+export const MyCampaignCard: React.FC<MyCampaignCardProps> = ({ contractAddress }) => {
+    const contract = getContract({
+        client: client,
+        chain: lineaSepolia,
+        address: contractAddress,
+    });
+
+    // Get Campaign Name
+    const { data: name } = useReadContract({
+        contract, 
+        method: "function name() view returns (string)", 
+        params: []
+    });
+
+    const { data: description } = useReadContract({ 
+        contract, 
+        method: "function description() view returns (string)", 
+        params: [] 
+      });
+
+    return (
+            <div className="flex flex-col justify-between max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow">
+                <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight">{name}</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+                </div>
+                
+                <Link
+                    href={`/campaign/${contractAddress}`}
+                    passHref={true}
+                >
+                    <p className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        View Campaign
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </p>
+                </Link>
+            </div>
+    )
+};
