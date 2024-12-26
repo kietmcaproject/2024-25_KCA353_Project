07cc988f993a68fd09f7ffa91bd9037a0858c709
meenakshi2325mca <meenakshi.2325mca100@kiet.edu>
2024-12-26 15:54:30 +0000

    Added All Related Documents

---
+++ GB09_ChaloRidePe/Project_Code_GB09/components/GoogleTextInput.tsx
@@ -0,0 +1,18 @@
+import { GoogleInputProps } from "@/types/type";
+import { Text, View } from "react-native";
+
+const GoogleTextInput = ({
+  icon,
+  initialLocation,
+  containerStyle,
+  textInputBackgroundColor,
+  handlePress,
+}: GoogleInputProps) => (
+  <View
+    className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
+  >
+    <Text>search</Text>
+  </View>
+);
+
+export default GoogleTextInput;
---
+++ GB09_ChaloRidePe/Project_Code_GB09/components/OAuth.tsx
@@ -0,0 +1,52 @@
+import { Alert, Image, Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+import { icons } from "@/constants";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useCallback } from "react";
+import { googleOAuth } from "@/lib/auth";
+import { router } from "expo-router";
+
+const OAuth = () => {
+  // const handleGoogleSignIn = async () => {};
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleSignIn = useCallback(async () => {
+    try {
+      const result = await googleOAuth(startOAuthFlow);
+
+      if (result.code === "session_exists") {
+        Alert.alert("Success", "Session Exists. Redirecting to home page");
+        router.push("/(root)/(tabs)/home");
+      }
+
+      Alert.alert(result.success ? "Success" : "Error", result.message);
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
+  return (
+    <View>
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+        <View className="flex-1 h-[1px] bg-general-100" />
+        <Text className="text-lg">Or</Text>
+        <View className="flex-1 h-[1px] bg-general-100" />
+      </View>
+      <CustomButton
+        title="Log In with Google"
+        className="mt-5 w-full shadow-none"
+        IconLeft={() => (
+          <Image
+            source={icons.google}
+            resizeMode="contain"
+            className="w-5 h-5 mx-2"
+          ></Image>
+        )}
+        bgVariant="outline"
+        textVariant="primary"
+        onPress={handleGoogleSignIn}
+      ></CustomButton>
+    </View>
+  );
+};
+export default OAuth;
